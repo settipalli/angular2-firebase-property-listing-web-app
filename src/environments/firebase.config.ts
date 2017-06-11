5b718e358b70e9c8b5b9518d4e90b6d8521de299
Santhoshkumar Settipalli <santhosh.settipalli@gmail.com>
2017-06-12 01:47:46 +0530

    Update firebase configuration with comments.

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,3 +1,5 @@
+// Update this file with firebase configuration of your project
+// https://stackoverflow.com/questions/4348590/how-can-i-make-git-ignore-future-revisions-to-a-file
 export const config = {
   apiKey: "",
   authDomain: "",
