295d9fa14fb72d0e7fac67405e1f96bf564d3edc
Santhoshkumar Settipalli <santhosh.settipalli@gmail.com>
2017-06-11 19:54:51 +0530

    Added Home and Navbar components. Added Cosmos Bootstrap theme.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {RouterModule, Routes} from "@angular/router";
 
-import { AppComponent } from './app.component';
+import {AppComponent} from "./app.component";
+import {HomeComponent} from "./component/home/home.component";
+import { NavbarComponent } from './component/navbar/navbar.component';
+
+const appRoutes: Routes = [{
+  path: '', component: HomeComponent
+}]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
